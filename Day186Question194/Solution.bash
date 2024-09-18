c969f4a9d992a2f3e1f196bb7782bc17fe027a35
not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>
2024-09-18 20:25:44 +0545

    Add files via upload
    Signed-off-by: not ∞ <110843279+RUPAKparajuli07@users.noreply.github.com>

---
+++ Day186Question194/Solution.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Transpose the file content
+awk '
+{
+    for (i=1; i<=NF; i++)  {
+        if (NR == 1) {
+            arr[i] = $i
+        } else {
+            arr[i] = arr[i] " " $i
+        }
+    }
+}
+END {
+    for (i=1; i<=NF; i++) {
+        print arr[i]
+    }
+}
+' file.txt
